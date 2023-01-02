20b9139c0ca961820d2496d5a7e3446f2a9bdb93
LinTongXue <LinTongXue@lin.com>
2023-01-03 00:13:17 +0800

    完成项目的创建

---
+++ src/main/java/com/word/WordSpotCheckApplication.java
@@ -0,0 +1,13 @@
+package com.word;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordSpotCheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WordSpotCheckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/word/WordSpotCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.word;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordSpotCheckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
